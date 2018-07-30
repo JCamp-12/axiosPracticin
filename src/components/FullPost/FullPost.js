c8fc8124f602ad92a6e183670aebc9c18ebb96df
JCamp-12 <JChandlerCampbell@gmail.com>
2018-07-30 02:15:22 -0400

    fetching data on update without the infinite loop this time

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -3,31 +3,49 @@ import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+
     componentDidUpdate () {
-        if(this.props.id){
-            axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
-            .then(response => {
-                // console.log(response.data.id)
-                console.log(response.data.body)
-                
-        })
+        if ( this.props.id ) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+                    .then(response => {
+                    // console.log(response.data)
+                    this.setState({ loadedPost: response.data})
+                    // console.log(response.data.id)
+                    // console.log(response.data.body)
+                    // console.log(response.data.title)
+                    // this.setState({ loadedPost: response.data})
+                })
+            }
+            
         }
         
     }
 
     render () {
+        console.log('this is prop id ' + this.props.id)
+        console.log('this is loadedPost: ' , this.state.loadedPost)
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if(this.props.id) {
+            let post = <p style={{textAlign: 'center'}}>Loading...</p>;
+            
+        } 
+        // console.log(post)
+        if ( this.state.loadedPost) {
         post = (
             <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
+                <h1>{this.state.loadedPost.title}</h1>
+                <p>{this.state.loadedPost.body}</p>
                 <div className="Edit">
                     <button className="Delete">Delete</button>
                 </div>
             </div>
 
         );
+        
         }
         return post;
     
